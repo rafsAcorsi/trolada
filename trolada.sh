00c6416d90b0122a62db6b0d77ab9daf6a28a260
Rafs <rafaelacorsi@gmail.com>
2017-08-03 15:12:03 -0300

    ok

--- trolada.sh
+++ trolada.sh
@@ -1,6 +1,6 @@
 install() {
 	curl -L#o /var/tmp/lerolero.png https://static.omelete.uol.com.br/media/extras/conteudos/painel-painel-hulk-1-80-x-1-20m-painel-festa.jpg
-	echo "open /var/tmp/lerolero.png" >> ~/.bash_profile
+	echo "open /var/tmp/lerolero.png" >> ~/.bashr
         source ~/.bashrc
 }
 
